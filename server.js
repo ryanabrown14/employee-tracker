7341ff50fdee3623cc31f7a907db7016b8942de9
ry brown <ryan.alex.brown@gmail.com>
2020-10-17 00:16:54 -0700

    more basic functions

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const cTable = require('console.table');
 const inquirer = require('inquirer');
 
 
+
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'Panic!14',
-    database: 'employee_infoDB'
+    database: 'employee_infodb'
   });
 
 connection.connect(err => {
@@ -33,11 +34,42 @@ afterConnection = () => {
            'Update Employee Manager',
            'Quit'
        ]
-    }).then(response =>{
+    }).then(choices =>{
+        if ('View all Employees'){
+            viewEmployees();
+        }
+        else if ('View all Employees by Department'){
+            viewDepartment();
+        }
+        else if ('View all Employees by Manager'){
+            viewManager();
+        }
+        else if ('Add Employee'){
+            addEmployee();
+        }
+        else if ('Update Employee Role'){
+            updateRole();
+        }
+        else if ('Update Employee Manager'){
+            updateManager();
+        }
+        else {
+            end();
+        }
+       
 
     });
 
-   
-    
 
 };
+
+function viewEmployees() {
+    //console.log('this works')
+    let query = "SELECT * FROM employee;";
+    connection.query(query, function(err, res) {
+    if (err) throw err;
+    console.log(res.length + " employees found!");
+    console.table('All Employees:', res); 
+    
+    })
+}
